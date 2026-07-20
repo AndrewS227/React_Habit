2e075c548474fc3124dd01f9220dd25a38bc90f7
ANDREWS227 <jandris227@gmail.com>
2026-07-20 12:00:51 +0200

    Added input line and button to HabitForm function.

--- src/components/HabitForm.tsx
+++ src/components/HabitForm.tsx
@@ -1,3 +1,10 @@
+import { Button2 } from "./Button"
+
 export function HabitForm(){
-  return null
+  return (
+    <form className="flex gap-2">
+    <input className="flex-1 rounded-lg bg-zinc-800 px-4 py-2 outline-none focus-visible:ring-2 focus-visible:ring-red-500" placeholder="New Habit..." />
+    <Button2>Add Habit</Button2>
+    </form>
+  )
 }
