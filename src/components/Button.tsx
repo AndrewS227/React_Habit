c5f337cfc0766e90dbc335b4049d7a7c60809c54
ANDREWS227 <jandris227@gmail.com>
2026-07-21 12:12:34 +0200

    Added ComponentProps to ButtonProps2 which takes care of any prop. I would need. Deleted children prop. for this reason. Updated Button2 func. accordingly.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from "react"
+import type {ComponentProps} from "react"
 
 // props for custom use
 type ButtonProps = {
@@ -8,8 +8,10 @@ type ButtonProps = {
 // simpler props, children v.
 // React automatically handles children props!
 type ButtonProps2 = {
-    children: ReactNode
-}
+    //disabled?: boolean // ? -> optional prop.
+    //Listing all needed props. would be cumbersome,
+    //Use ComponentProps instead to reach every prop. of an object!
+} & ComponentProps<"button">
 
 // props
 export function Button(props: ButtonProps){
@@ -17,6 +19,11 @@ export function Button(props: ButtonProps){
 }
 
 // children props are always available for any custom component!
-export function Button2({children}: ButtonProps2){
-    return <button className="bg-indigo-900 hover:bg-indigo-800 transition-colors rounded px-2 py-1 disabled:opacity-30 disabled:cursor-not-allowed">{children}</button>
+export function Button2({...props}: ButtonProps2){
+    return (
+    <button
+    {...props}
+     className="bg-indigo-900 hover:bg-indigo-800 transition-colors rounded px-2 py-1 disabled:opacity-30 disabled:cursor-not-allowed"
+    />
+    )
 }
